ad59bba66627d5a915918e6aefb96e97dd156d0c
ichikawa-manami <manami.hori@fonfun.co.jp>
2024-05-28 16:06:41 +0900

    スキル名をDBのデータから持ってきて表示する機能の実装

--- src/main/java/com/example/demo/controller/ChartListViewController.java
+++ src/main/java/com/example/demo/controller/ChartListViewController.java
@@ -1,18 +1,38 @@
 package com.example.demo.controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.demo.constant.UrlConst;
+import com.example.demo.dto.LearningRequest;
+import com.example.demo.entity.LearningInfo;
+import com.example.demo.repository.UserInfoRepository;
+import com.example.demo.service.LearningInfoService;
+
+import lombok.RequiredArgsConstructor;
 
 @Controller
+@RequiredArgsConstructor
 public class ChartListViewController {
+	
+	private final UserInfoRepository userInfoRepository;
+	
+	private final LearningInfoService learningInfoService;
 
 	@GetMapping(UrlConst.CHARTLIST)
-	public String listviewdisplay(Model model) {
-		
+	public String listviewdisplay( Model model) {
+	    
+	    List<LearningInfo> skillName = learningInfoService.findAll();
+	    
+        model.addAttribute("skillName", skillName);
+        model.addAttribute("userSearchRequest", new LearningRequest());
+        
+	    
 		return "listview";
 	}
 	
--- src/main/java/com/example/demo/dao/UserInfoMapper.java
+++ src/main/java/com/example/demo/dao/UserInfoMapper.java
@@ -1,5 +1,7 @@
 package com.example.demo.dao;
+import java.util.List;
 import java.util.Optional;
+
 
 
 
@@ -8,6 +10,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 import com.example.demo.dto.IntroEditRequest;
 import com.example.demo.dto.UserAddRequest;
+import com.example.demo.entity.LearningInfo;
 import com.example.demo.entity.UserInfo;
 
 @Mapper
@@ -17,4 +20,12 @@ public interface UserInfoMapper {
 	public UserInfo findByEmail(String email);
 	
 	 void edit(IntroEditRequest introEditRequest);
+	 
+		public LearningInfo findBySkillName(String name);
+
+		List<LearningInfo> findAll();
+		
+		LearningInfo findId(Long id);
+
+		public LearningInfo findById(Long id);
 }
